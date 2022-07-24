8a2266a329cd66859e765e288a6ba3f6bbc46f1f
atulsharma <atulsharma@uni-koblenz.de>
2022-07-24 20:27:51 +0200

    Added documentation

--- sample_code.py
+++ sample_code.py
@@ -1,14 +1,17 @@
-def check_string_length(usr_input: str) -> int:
-    if not usr_input:
+"""
+Provides lenght of string function
+"""
+
+def find_lenght_of_string(string: str) -> int:
+    '''
+        finds the length of the string
+    '''
+
+    if not string:
         return 0
-    str_input = f'{usr_input}'
+    str_input = f'{string}'
     _str_length = len(str_input)
     return _str_length
 
-def make_complex(*args):
-    x, y = args
-    return dict(**locals())
-
-if __name__ == '__main__':
-    usr_input = 'Hello World'
-    check_string_length(usr_input)
+SAMPLE_STRING = 'hello world'
+find_lenght_of_string(SAMPLE_STRING)
