d3efcf11342ef65b781f8045609393b18503f7c3
atulsharma <atulsharma@uni-koblenz.de>
2022-07-24 20:11:26 +0200

    added new function make_complex

--- sample_code.py
+++ sample_code.py
@@ -5,6 +5,10 @@ def check_string_length(usr_input: str) -> int:
     _str_length = len(str_input)
     return _str_length
 
+def make_complex(*args):
+    x, y = args
+    return dict(**locals())
+
 if __name__ == '__main__':
     usr_input = 'Hello World'
     check_string_length(usr_input)
