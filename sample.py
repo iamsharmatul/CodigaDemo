eff993c7ee1bfbf1119e69e1c325c173c6170c67
atulsharma <atulsharma@uni-koblenz.de>
2022-07-24 20:55:06 +0200

    Some random code

--- sample.py
+++ sample.py
@@ -13,5 +13,10 @@ def find_lenght_of_string(string: str) -> int:
     _str_length = len(str_input)
     return _str_length
 
+# Some random code to check the codiga is working or not
+def make_complex(*args):
+    x, y = args
+    return dict(**locals())
+
 SAMPLE_STRING = 'hello world'
 find_lenght_of_string(SAMPLE_STRING)
